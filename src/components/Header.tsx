bfb08c2630416cf8e91d4b8effcfc31e7b2ac32e
kamimasu02 <kamimasu02@gmail.com>
2024-10-03 21:57:43 +0900

    feat: Header 미디어 쿼리 작업 및 모바일 햄버거 버튼, 메뉴 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import { Link } from "react-router-dom";
 
 import { HEADER_LINKS } from "@consts/header";
+
+import HamburgerButton from "@components/header/HamburgerButton";
 
 import {
   HeaderWrapper,
   MainHeader,
   HeaderLogo,
   HeaderNavBar,
+  HamburgerMenus,
+  HamburgerMenusBackground,
+  HamburgerMenusWrapper,
+  HamburgerMenu,
 } from "src/styles/header.style";
 
 import HeaderLogoImage from "@assets/images/logos/header-logo.png";
@@ -14,6 +22,28 @@ import HeaderLogoImage from "@assets/images/logos/header-logo.png";
 import type { FC } from "react";
 
 const Header: FC = () => {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  const handleOpenMenu = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!isMenuOpened) {
+      document.body.style.overflow = "hidden";
+      setIsMenuOpened(true);
+    }
+  };
+
+  const handleCloseMenu = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (isMenuOpened) {
+      document.body.style.overflow = "auto";
+      setIsMenuOpened(false);
+    }
+  };
+
   return (
     <HeaderWrapper>
       <MainHeader>
@@ -27,6 +57,22 @@ const Header: FC = () => {
             </Link>
           ))}
         </HeaderNavBar>
+        <HamburgerButton onClick={handleOpenMenu} />
+        {createPortal(
+          <HamburgerMenusWrapper className={isMenuOpened ? "show" : ""}>
+            <HamburgerMenusBackground onClick={handleCloseMenu} />
+            <HamburgerMenus className={isMenuOpened ? "show" : ""}>
+              {HEADER_LINKS.map(({ text, to }, index) => (
+                <Link key={`${index}-${text}-mobile`} to={to} reloadDocument>
+                  <HamburgerMenu>
+                    <span>{text}</span>
+                  </HamburgerMenu>
+                </Link>
+              ))}
+            </HamburgerMenus>
+          </HamburgerMenusWrapper>,
+          document.body
+        )}
       </MainHeader>
     </HeaderWrapper>
   );
--- src/styles/header.style.ts
+++ src/styles/header.style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import { COLOR, FONT, SCREEN_MAX_WIDTH } from "@consts/style";
+import { COLOR, FONT, SCREEN, SCREEN_MAX_WIDTH } from "@consts/style";
 
 export const HeaderWrapper = styled.div`
   display: flex;
@@ -15,11 +15,20 @@ export const MainHeader = styled.header`
   max-width: ${SCREEN_MAX_WIDTH};
   height: 70px;
   padding: 15px;
+
+  @media screen and (max-width: ${SCREEN.mobileWide}) {
+    height: 50px;
+  }
 `;
 
 export const HeaderLogo = styled.img`
   width: 70px;
   height: 70px;
+
+  @media screen and (max-width: ${SCREEN.mobileWide}) {
+    width: 50px;
+    height: 50px;
+  }
 `;
 
 export const HeaderNavBar = styled.nav`
@@ -34,5 +43,130 @@ export const HeaderNavBar = styled.nav`
     width: fit-content;
     height: fit-content;
     margin: auto;
+  }
+
+  @media screen and (max-width: ${SCREEN.tablet}) {
+    display: none;
+  }
+`;
+
+// 모바일 햄버거 버튼
+
+export const HeaderHamburgerButton = styled.button`
+  display: none;
+  background: transparent;
+  border: 0;
+
+  & > img {
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: ${SCREEN.tablet}) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto 0;
+    width: fit-content;
+    height: fit-content;
+  }
+`;
+
+export const HamburgerMenusWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+
+  &.show {
+    z-index: 2;
+
+    :first-child {
+      visibility: visible;
+    }
+  }
+`;
+
+export const HamburgerMenusBackground = styled.div`
+  width: 100%;
+  height: calc(100% - 175px);
+  background-color: ${COLOR.black};
+  visibility: hidden;
+  transition: visibility 0.2s ease-in;
+  opacity: 0.3;
+
+  @media screen and (max-width: ${SCREEN.mobileWide}) {
+    height: calc(100% - 120px);
+  }
+`;
+
+export const HamburgerMenus = styled.ul`
+  display: none;
+  max-width: 0;
+  font-size: 0;
+  transition: max-width 0.2s ease-in, font-size 0.2s ease-in;
+
+  @media screen and (max-width: ${SCREEN.tablet}) {
+    display: flex;
+    position: absolute;
+    flex-direction: column;
+    align-items: center;
+    width: 120px;
+    height: calc(100% - 175px);
+    top: 0;
+    right: 0;
+    background-color: ${COLOR.subColor1};
+    color: ${COLOR.mainColor};
+    font-family: "Pretendard";
+    font-weight: 500;
+    list-style: none;
+
+    & > a {
+      display: none;
+      width: 100%;
+    }
+
+    &.show {
+      max-width: 120px;
+      font-size: ${FONT.tablet.normal};
+
+      & > a {
+        display: flex;
+      }
+    }
+  }
+
+  @media screen and (max-width: ${SCREEN.mobileWide}) {
+    width: 100px;
+    height: calc(100% - 120px);
+
+    &.show {
+      max-width: 100px;
+      font-size: ${FONT.mobile.normal};
+    }
+  }
+
+  @media screen and (max-width: ${SCREEN.mobileNarrow}) {
+    width: 80px;
+
+    &.show {
+      max-width: 80px;
+      font-size: 12px;
+    }
+  }
+`;
+
+export const HamburgerMenu = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  word-break: keep-all;
+
+  &:hover {
+    background-color: #b8aee7;
+    color: ${COLOR.white};
   }
 `;
