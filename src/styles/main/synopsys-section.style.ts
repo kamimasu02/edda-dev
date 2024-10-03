02fa6adef95c1c110b85d17d04a6cdf66e74b29c
kamimasu02 <kamimasu02@gmail.com>
2024-10-03 13:15:51 +0900

    style: 시놉시스 영역 미디어 쿼리 작업

--- src/styles/main/synopsys-section.style.ts
+++ src/styles/main/synopsys-section.style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import { FONT } from "@consts/style";
+import { COLOR, FONT, SCREEN } from "@consts/style";
 
 export const SynopsysWrapper = styled.section<{
   $image: string;
@@ -21,13 +21,75 @@ export const SynopsysWrapper = styled.section<{
       1 - $opacity
     })), url(${$image})`};
   background-size: cover;
+
+  @media screen and (max-width: ${SCREEN.tablet}) {
+    padding: 100px 160px;
+  }
+
+  @media screen and (max-width: ${SCREEN.mobileWide}) {
+    padding: 120px 16px;
+    row-gap: 60px;
+  }
+`;
+
+export const SynopsysTitleWrapper = styled.div`
+  display: flex;
+  position: relative;
+  justify-content: center;
+  padding: 0 8px;
+
+  @media screen and (max-width: ${SCREEN.mobileWide}) {
+    padding: 0 15px;
+  }
+`;
+
+export const SynopsysTitleBackground = styled.div`
+  position: absolute;
+  bottom: 0;
+  background-color: ${COLOR.subColor1};
+  width: 100%;
+  height: 16px;
+
+  @media screen and (max-width: ${SCREEN.mobileWide}) {
+    height: 10px;
+  }
+
+  @media screen and (max-width: 359px) {
+    max-width: 160px;
+  }
 `;
 
 export const SynopsysTitle = styled.span`
+  position: inherit;
   font-family: "Pretendard";
   font-size: ${FONT.pc.title};
   font-weight: 900;
   line-height: 44px;
+  z-index: 2;
+
+  @media screen and (max-width: ${SCREEN.tablet}) {
+    font-size: ${FONT.tablet.title};
+  }
+
+  @media screen and (max-width: ${SCREEN.mobileWide}) {
+    font-size: ${FONT.mobile.title};
+    line-height: 24px;
+  }
+
+  &.default {
+    @media screen and (max-width: 359px) {
+      display: none;
+    }
+  }
+
+  &.under-mobile {
+    display: none;
+
+    @media screen and (max-width: 359px) {
+      display: flex;
+      flex-direction: column;
+    }
+  }
 `;
 
 export const SynopsysParagraph = styled.p`
@@ -39,8 +101,32 @@ export const SynopsysParagraph = styled.p`
   font-size: ${FONT.pc.normal};
   font-weight: 500;
   line-height: 28px;
+
+  @media screen and (max-width: ${SCREEN.tablet}) {
+    font-size: ${FONT.tablet.normal};
+  }
+
+  @media screen and (max-width: ${SCREEN.mobileWide}) {
+    font-size: ${FONT.mobile.normal};
+    line-height: 26px;
+  }
 `;
 
 export const SynopsysText = styled.span`
   text-align: center;
+  word-break: keep-all;
+
+  &.default {
+    @media screen and (max-width: 359px) {
+      display: none;
+    }
+  }
+
+  &.under-mobile {
+    display: none;
+
+    @media screen and (max-width: 359px) {
+      display: inline;
+    }
+  }
 `;
