3d4d1ee6d5e647ac5dec8d397d0082f28e1a238a
kamimasu02 <kamimasu02@gmail.com>
2024-10-03 13:15:51 +0900

    style: 시놉시스 영역 미디어 쿼리 작업

--- src/components/main/SynopsysSection.tsx
+++ src/components/main/SynopsysSection.tsx
@@ -5,6 +5,8 @@ import {
   SynopsysWrapper,
   SynopsysParagraph,
   SynopsysText,
+  SynopsysTitleWrapper,
+  SynopsysTitleBackground,
 } from "@styles/main/synopsys-section.style";
 
 import SynopsysImage from "@assets/images/main/synopsys-background.png";
@@ -23,9 +25,18 @@ const SynopsysSection: FC = () => {
         }}
         direction="left"
       />
-      <SynopsysTitle>낙천한 신들의 아이돌 도전기!</SynopsysTitle>
+      <SynopsysTitleWrapper>
+        <SynopsysTitle className="default">
+          낙천한 신들의 아이돌 도전기!
+        </SynopsysTitle>
+        <SynopsysTitle className="under-mobile">
+          <span>낙천한 신들의</span>
+          <span>아이돌 도전기!</span>
+        </SynopsysTitle>
+        <SynopsysTitleBackground />
+      </SynopsysTitleWrapper>
       <SynopsysParagraph>
-        <SynopsysText>
+        <SynopsysText className="default">
           'IDOL'우상이란 본디 신의 형상을 의미한다.
           <br />
           800만은 가볍게 넘어가는 신들이 인간계로 추락한 어떤 세계.
@@ -34,6 +45,24 @@ const SynopsysSection: FC = () => {
           <br />
           <br />
           그리고 신들이 하늘 위에 군림하지 않고 인간의 곁에서 숭배당하는 세계.
+          <br />
+          매니저, 우상(偶像)이자 우상(IDOL)인 신들을 이끌어주기를...
+        </SynopsysText>
+        <SynopsysText className="under-mobile">
+          'IDOL'우상이란 본디
+          <br />
+          신의 형상을 의미한다.
+          <br />
+          800만은 가볍게 넘어가는 신들이
+          <br />
+          인간계로 추락한 어떤 세계.
+          <br />
+          신앙 아래에 신이 존재하는 세계.
+          <br />
+          <br />
+          그리고 신들이 하늘 위에 군림하지 않고
+          <br />
+          인간의 곁에서 숭배당하는 세계.
           <br />
           매니저, 우상(偶像)이자 우상(IDOL)인 신들을 이끌어주기를...
         </SynopsysText>
